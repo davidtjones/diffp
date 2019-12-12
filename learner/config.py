bug fixes, spectral normalization

learner/config.py
@@ -4,17 +4,18 @@ import torch
 import torch.nn as nn
 
 class Config:
-    def __init__(self, epochs, batch_size, learning_rate, device_num=0):
+    def __init__(self, epochs, batch_size, learning_rateD, learning_rateG, device_num=0):
         self.name = "Diabetic Retinopathy with Uncertainty"
         self.device = torch.device("cuda:%s" % device_num if torch.cuda.is_available() else "cpu")
         self.device_name = torch.cuda.get_device_name(device_num) if torch.cuda.is_available() else "cpu"
 
         self.num_classes = 2
-        self.num_workers = 8
+        self.num_workers = 10
 
         self.epochs = epochs
         self.batch_size = batch_size
-        self.lr = learning_rate
+        self.lrD = learning_rateD
+        self.lrG = learning_rateG
 
         self.scheduler = None
 

learner/model.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
-
+from torch.nn.utils import spectral_norm
 
 # custom weights initialization called on netG and netD
 def weights_init(m):
@@ -75,23 +75,24 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            # input is (nc) x 64 x 64
-            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            # maybe try removing batch normalizations
+            # input is (nc) x 64 x 64j
+            spectral_norm(nn.Conv2d(nc, ndf, 4, 2, 1, bias=False)),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 2),
+            spectral_norm(nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False)),
+            #nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
+            spectral_norm(nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False)),
+            #nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
+            spectral_norm(nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False)),
+            #nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            spectral_norm(nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False)),
             nn.Sigmoid()
         )
 

learner/train.py
@@ -45,13 +45,13 @@ def train(dataset, config, test_dataset=None,
     #    netD.load_state_dict(torch.load(opt.netD))
     # print(netD)
     
-    optimizerD = optim.Adam(netD.parameters(), lr=config.lr, betas=(0.5, 0.999))
-    optimizerG = optim.Adam(netG.parameters(), lr=config.lr, betas=(0.5, 0.999))
+    optimizerD = optim.Adam(netD.parameters(), lr=config.lrD, betas=(0.5, 0.999))
+    optimizerG = optim.Adam(netG.parameters(), lr=config.lrG, betas=(0.5, 0.999))
 
     fixed_noise = torch.randn(config.batch_size, config.nz, 1, 1, device=config.device)
 
-    real_label = 1
-    fake_label = 0
+    real_label = .9
+    fake_label = .1
     criterion = nn.BCELoss()
 
     iterations = 0
@@ -79,32 +79,34 @@ def train(dataset, config, test_dataset=None,
             
             # Train with all-real batch
             images = batch['image'].to(config.device)
-            
+
+            # Show a sample of real images
+            if batch_idx == 0 and epoch == 0:
+                tb.add_image('sample images', vutils.make_grid(images, padding=2, normalize=True), epoch)
+
             b_size = images.size(0)
             # labels = batch['label'].to(config.device)
             # labels for GAN are 1 for a real image and 0 for a fake image
             # label = torch.full((b_size,), real_label, device=config.device)
             # Label smoothing: 
-            label = np.random.random_sample((b_size,))
-            label_real = label*(.5) + .7
-            label_real = torch.tensor(label_real, device=config.device).float()
+            label = torch.full((b_size,), real_label, device=config.device)
 
-            label_fake = label * .3
-            label_fake = torch.tensor(label_fake, device=config.device).float()
             
             ## Update D Network
             # Train with all real batch         
             output = netD(images).view(-1)
             # noisy labels: (flip occassionally)
-            errD_real = criterion(output, label_real)
+            errD_real = criterion(output, label)
             errD_real.backward()
             D_x = output.mean().item()
             
             ## Train with all fake batch
+
             noise = torch.randn(b_size, config.nz, 1, 1, device=config.device)
             fake = netG(noise)
+            label = label.fill_(fake_label)
             output = netD(fake.detach()).view(-1)
-            errD_fake = criterion(output, label_fake)
+            errD_fake = criterion(output, label)
                 
             errD_fake.backward()
             D_G_z1 = output.mean().item()
@@ -113,8 +115,9 @@ def train(dataset, config, test_dataset=None,
             
             ## Update G Network
             netG.zero_grad()
+            label.fill_(real_label)
             output = netD(fake).view(-1)
-            errG = criterion(output, label_fake)
+            errG = criterion(output, label)
             errG.backward()
             D_G_z2 = output.mean().item()
             optimizerG.step()
@@ -123,10 +126,16 @@ def train(dataset, config, test_dataset=None,
             # Output training stats
             if batch_idx % 50 == 0:
                 print(f'[{epoch}/{config.epochs}][{batch_idx}/{len(dataloader)}]\tLoss_D: {errD.item()}\tLoss_G: {errG.item()}, D(x): {D_x}, D(G(z)): {D_G_z1} / {D_G_z2}')
+                with torch.no_grad():
+                    fake = netG(fixed_noise).detach().cpu()
+                tb.add_image('generated images',vutils.make_grid(fake, padding=2, normalize=True), epoch)
+                losses["loss_D"] = errD.item()
+                losses["loss_G"] = errG.item()
+                tb.add_to_metrics_plot(epoch, losses)                
                 
             
-            running_loss_G += errG.item() * label_real.shape[0]
-            running_loss_D += errD.item() * label_real.shape[0]
+            running_loss_G += errG.item() * label.shape[0]
+            running_loss_D += errD.item() * label.shape[0]
                                 
             iterations += 1
             
@@ -134,14 +143,6 @@ def train(dataset, config, test_dataset=None,
         running_loss_D /= dataset_size
         running_loss_G /= dataset_size
 
-        losses["loss_D"] = running_loss_D
-        losses["loss_G"] = running_loss_G
-        tb.add_to_metrics_plot(epoch, losses)
-
-        # Generate sample generated images after each epoch
-        with torch.no_grad():
-            fake = netG(fixed_noise).detach().cpu()
-        tb.add_image(vutils.make_grid(fake, padding=2, normalize=True), epoch)
-
+        print(f"Epoch loss: D: {running_loss_D}\t G: {running_loss_G}")
 
 